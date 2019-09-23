5587f3c9e257641104ceb75fad969fb67af6be67
Jason Lui <mrjasonlui@gmail.com>
2019-09-23 12:50:49 -0700

    Removed repeated code

--- A1/lotto.py
+++ A1/lotto.py
@@ -7,28 +7,9 @@ def number_generator():
     :return: None
     """
     import random
-    res = []
-    nums = list(range(50))
 
-    # Generates a number not in res and adds it to res
-    n1 = random.choice(nums)
-    res.append(n1)
-    nums.remove(n1)
-    n2 = random.choice(nums)
-    res.append(n2)
-    nums.remove(n2)
-    n3 = random.choice(nums)
-    res.append(n3)
-    nums.remove(n3)
-    n4 = random.choice(nums)
-    res.append(n4)
-    nums.remove(n4)
-    n5 = random.choice(nums)
-    res.append(n5)
-    nums.remove(n5)
-    n6 = random.choice(nums)
-    res.append(n6)
-    nums.remove(n6)
+    # Generates a sample of 6 numbers from [1, 49]
+    res = random.sample(list(range(50)), 6)
 
     # Sorts res in ascending order
     res.sort()
@@ -36,6 +17,7 @@ def number_generator():
     # Prints res in ascending order in a single line
     print(res[0], res[1], res[2], res[3], res[4], res[5])
     return
+
 
 # Calls the main function
 if __name__ == '__main__':
