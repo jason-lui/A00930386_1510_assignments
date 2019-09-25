16bacb482e6dddbba45da8783fa8c336ec1877b2
Jason Lui <mrjasonlui@gmail.com>
2019-09-25 00:06:21 -0700

    Style

--- A1/random_game.py
+++ A1/random_game.py
@@ -1,9 +1,8 @@
 def rock_paper_scissors():
     """
-    Plays a game of rock paper scissors.
+    Play a game of rock paper scissors.
 
     User inputs rock paper scissors and battles against a randomly generated move.
-    :return: None
     """
     # User inputs a string which is stripped and converted to lowercase
     choice = input("Enter rock paper or scissors: ").strip().lower()
