ad517e574e3e05b5b781b475ed2e9f1a4eccd19e
Jason Lui <mrjasonlui@gmail.com>
2019-10-15 13:13:19 -0700

    Copied main function in game_driver.py from lab 5.

--- A2/dungeonsanddragons.py
+++ A2/dungeonsanddragons.py
@@ -333,9 +333,60 @@ def attack(attacker, target):
 
 def main():
     """
-
+    !!!
     :return:
     """
+
+    # Ask user for the length of their username
+    name_length = int(input("Enter the length of your username: "))
+
+    # create_character() calls roll_die() and create_name()
+    # create_name() calls generate_syllable()
+    # generate_syllable() calls generate_consonant() and generate_vowel()
+    # Inform the user that their stats are being rolled
+    print("Rolling for strength, dexterity, constitution, intelligence, wisdom, charisma...")
+    character = lab05.create_character(name_length)
+
+    # Show rolls
+    print(f"You rolled: {character[1][1]}, {character[2][1]}, {character[3][1]}, {character[4][1]}, {character[5][1]}, {character[6][1]}!")
+    print("\n")
+
+    # A list of possible items in the game
+    item_list = ['Rabadon\'s Deathcap',
+                 'Seraph\'s Embrace',
+                 'Liandry\'s Torment',
+                 'Morellonomicon',
+                 'Warmog\'s Armor',
+                 'Randuin\'s Omen',
+                 'Banshee\'s Veil',
+                 'Abyssal\' Mask',
+                 'Infinity Edge',
+                 'The Bloodthirster',
+                 'Phantom Dancer',
+                 'Rapidfire Cannon',
+                 'Boots of Lucidity',
+                 'Boots of Swiftness',
+                 'Boots of Alacrity',
+                 'Mercury Treads']
+
+    # Show items in the store
+    print("--Available items in the store--")
+    for item in item_list:
+        print(item)
+    print("\n")
+
+    # Ask user for how many items they would like
+    num_of_items = int(input("How many items would you like? (MAX 16): "))
+    print("\n")
+    print("----------------------------------------------------")
+
+    # Choose items for the character's inventory using choose_inventory()
+    character_items = lab05.choose_inventory(item_list, num_of_items)
+    character.append(character_items)
+
+    # Print character info
+    lab05.print_character(character)
+    return
 
 
 
