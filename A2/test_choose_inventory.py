987fe27a176370009dfa89545302393b28acd39b
Jason Lui <mrjasonlui@gmail.com>
2019-10-18 11:48:23 -0700

    Added 3 more unit tests

--- A2/test_choose_inventory.py
+++ A2/test_choose_inventory.py
@@ -9,7 +9,7 @@ class TestChoose_inventory(TestCase):
     # Choose no items
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("builtins.input", side_effect=[-1])
-    def test_choose_inventory_no_items(self, mock_items, mock_output):
+    def test_choose_inventory_no_items_print(self, mock_items, mock_output):
         sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         expected = """Welcome to the Olde Tyme Merchant!
 
@@ -22,6 +22,54 @@ Here is what we have for sale:
 3. Rapidfire Cannon
 4. Boots of Lucidity
 --Inventory--
+"""
+        inventory = choose_inventory(sample)
+        self.assertEqual(expected, mock_output.getvalue())
+
+    # Choose no items
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", side_effect=[-1])
+    def test_choose_inventory_no_items_return(self, mock_items, mock_output):
+        sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
+        expected = []
+        inventory = choose_inventory(sample)
+        self.assertEqual(expected, inventory)
+
+    # Choose multiple of the same item
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", side_effect=[1, 1, -1])
+    def test_choose_inventory_duplicate_print(self, mock_items, mock_output):
+        sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
+        expected = """Welcome to the Olde Tyme Merchant!
+
+Here is what we have for sale:
+--Current Inventory--
+
+
+1. Rabadon's Deathcap
+2. Abyssal Mask
+3. Rapidfire Cannon
+4. Boots of Lucidity
+--Current Inventory--
+Rabadon's Deathcap
+
+
+1. Rabadon's Deathcap
+2. Abyssal Mask
+3. Rapidfire Cannon
+4. Boots of Lucidity
+--Current Inventory--
+Rabadon's Deathcap
+Rabadon's Deathcap
+
+
+1. Rabadon's Deathcap
+2. Abyssal Mask
+3. Rapidfire Cannon
+4. Boots of Lucidity
+--Inventory--
+Rabadon's Deathcap
+Rabadon's Deathcap
 """
         inventory = choose_inventory(sample)
         self.assertEqual(expected, mock_output.getvalue())
@@ -29,7 +77,16 @@ Here is what we have for sale:
     # Choose multiple of the same item
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("builtins.input", side_effect=[1, 1, -1])
-    def test_choose_inventory_duplicate(self, mock_items, mock_output):
+    def test_choose_inventory_duplicate_return(self, mock_items, mock_output):
+        sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
+        expected = ['Rabadon\'s Deathcap', 'Rabadon\'s Deathcap']
+        inventory = choose_inventory(sample)
+        self.assertEqual(expected, inventory)
+
+    # Choose all items
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", side_effect=[1, 2, 3, 4, -1])
+    def test_choose_inventory_all_items_print(self, mock_items, mock_output):
         sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         expected = """Welcome to the Olde Tyme Merchant!
 
@@ -51,7 +108,28 @@ Rabadon's Deathcap
 4. Boots of Lucidity
 --Current Inventory--
 Rabadon's Deathcap
+Abyssal Mask
+
+
+1. Rabadon's Deathcap
+2. Abyssal Mask
+3. Rapidfire Cannon
+4. Boots of Lucidity
+--Current Inventory--
 Rabadon's Deathcap
+Abyssal Mask
+Rapidfire Cannon
+
+
+1. Rabadon's Deathcap
+2. Abyssal Mask
+3. Rapidfire Cannon
+4. Boots of Lucidity
+--Current Inventory--
+Rabadon's Deathcap
+Abyssal Mask
+Rapidfire Cannon
+Boots of Lucidity
 
 
 1. Rabadon's Deathcap
@@ -60,7 +138,9 @@ Rabadon's Deathcap
 4. Boots of Lucidity
 --Inventory--
 Rabadon's Deathcap
-Rabadon's Deathcap
+Abyssal Mask
+Rapidfire Cannon
+Boots of Lucidity
 """
         inventory = choose_inventory(sample)
         self.assertEqual(expected, mock_output.getvalue())
@@ -68,64 +148,11 @@ Rabadon's Deathcap
     # Choose all items
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("builtins.input", side_effect=[1, 2, 3, 4, -1])
-    def test_choose_inventory_4_items(self, mock_items, mock_output):
+    def test_choose_inventory_all_items_return(self, mock_items, mock_output):
         sample = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
-        expected = """Welcome to the Olde Tyme Merchant!
-
-Here is what we have for sale:
---Current Inventory--
-
-
-1. Rabadon's Deathcap
-2. Abyssal Mask
-3. Rapidfire Cannon
-4. Boots of Lucidity
---Current Inventory--
-Rabadon's Deathcap
-
-
-1. Rabadon's Deathcap
-2. Abyssal Mask
-3. Rapidfire Cannon
-4. Boots of Lucidity
---Current Inventory--
-Rabadon's Deathcap
-Abyssal Mask
-
-
-1. Rabadon's Deathcap
-2. Abyssal Mask
-3. Rapidfire Cannon
-4. Boots of Lucidity
---Current Inventory--
-Rabadon's Deathcap
-Abyssal Mask
-Rapidfire Cannon
-
-
-1. Rabadon's Deathcap
-2. Abyssal Mask
-3. Rapidfire Cannon
-4. Boots of Lucidity
---Current Inventory--
-Rabadon's Deathcap
-Abyssal Mask
-Rapidfire Cannon
-Boots of Lucidity
-
-
-1. Rabadon's Deathcap
-2. Abyssal Mask
-3. Rapidfire Cannon
-4. Boots of Lucidity
---Inventory--
-Rabadon's Deathcap
-Abyssal Mask
-Rapidfire Cannon
-Boots of Lucidity
-"""
+        expected = ['Rabadon\'s Deathcap', 'Abyssal Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         inventory = choose_inventory(sample)
-        self.assertEqual(expected, mock_output.getvalue())
+        self.assertEqual(expected, inventory)
 
     # Choose a number outside of the list
     @patch("sys.stdout", new_callable=io.StringIO)
