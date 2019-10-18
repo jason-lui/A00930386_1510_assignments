e3034cf2d5e59ae77c4a94d6cd16f5e1dc392ad9
Jason Lui <mrjasonlui@gmail.com>
2019-10-18 12:09:33 -0700

    Added 10 unit tests

--- A2/test_select_race.py
+++ A2/test_select_race.py
@@ -25,7 +25,63 @@ class TestSelect_race(TestCase):
 
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("builtins.input", return_value=1)
-    def test_select_race(self, mock_race, mock_output):
-        expected = "fighter"
+    def test_select_race_elf(self, mock_race, mock_output):
+        expected = "elf"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=2)
+    def test_select_race_halfling(self, mock_race, mock_output):
+        expected = "halfling"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=3)
+    def test_select_race_tiefling(self, mock_race, mock_output):
+        expected = "tiefling"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=4)
+    def test_select_race_dragonborn(self, mock_race, mock_output):
+        expected = "dragonborn"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=5)
+    def test_select_race_dwarf(self, mock_race, mock_output):
+        expected = "dwarf"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=6)
+    def test_select_race_gnome(self, mock_race, mock_output):
+        expected = "gnome"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=7)
+    def test_select_race_halfelf(self, mock_race, mock_output):
+        expected = "half-elf"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=8)
+    def test_select_race_human(self, mock_race, mock_output):
+        expected = "human"
+        select_race()
+        self.assertEqual(expected, select_race())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=9)
+    def test_select_race_halforc(self, mock_race, mock_output):
+        expected = "half-orc"
         select_race()
         self.assertEqual(expected, select_race())
