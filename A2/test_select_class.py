cf50aa8323e22cebe06a25d65dd906ac3ae0be17
Jason Lui <mrjasonlui@gmail.com>
2019-10-18 11:06:29 -0700

    Initial commit

--- A2/test_select_class.py
+++ A2/test_select_class.py
@@ -1,6 +1,27 @@
 from unittest import TestCase
-
+from unittest.mock import patch
+from dungeonsanddragons import select_class
+import io
 
 class TestSelect_class(TestCase):
-    def test_select_class(self):
-        self.fail()
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("builtins.input", return_value=1)
+    def test_select_class_fighter(self, mock_class, mock_output):
+        selected_class = "fighter"
+        expected = """Select a class:
+                   "1. Fighter
+                   "2. Wizard
+                   "3. Cleric
+                   "4. Rogue
+                   "5. Ranger
+                    6. Barbarian
+                    7. Bard
+                    8. Druid
+                    9. Monk
+                    10. Paladin
+                    11. Sorcerer
+                    12. Warlock"""
+        select_class()
+        self.assertEqual(expected, mock_output.getvalue())
+
