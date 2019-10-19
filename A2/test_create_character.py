831e8121e6c2704ddbcbac555935f98ace7a2984
Jason Lui <mrjasonlui@gmail.com>
2019-10-18 20:20:35 -0700

    Added 1 unit test

--- A2/test_create_character.py
+++ A2/test_create_character.py
@@ -5,26 +5,17 @@ from dungeonsanddragons import create_character
 
 class TestCreate_character(TestCase):
 
-    @patch("dungeonsanddragons.roll_die", side_effect=[1, 2, 3, 4, 5, 6])
-    @patch("random.choice", side_effect=["b", "a", "d", "e", "h", "i"])
-    def test_create_character_1(self, mock_letter, mock_roll):
-        syllables = 1
-        expected = ["Ba", ["Strength", 1], ["Dexterity", 2], ["Constitution", 3],
-                              ["Intelligence", 4], ["Wisdom", 5], ["Charisma", 6]]
-        self.assertEqual(expected, create_character(syllables))
-
-    @patch("dungeonsanddragons.roll_die", side_effect=[6, 5, 4, 3, 2, 1])
-    @patch("random.choice", side_effect=["b", "a", "d", "e", "h", "i"])
-    def test_create_character_2(self, mock_letter, mock_roll):
-        syllables = 2
-        expected = ["Bade", ["Strength", 6], ["Dexterity", 5], ["Constitution", 4],
-                              ["Intelligence", 3], ["Wisdom", 2], ["Charisma", 1]]
-        self.assertEqual(expected, create_character(syllables))
-
-    @patch("dungeonsanddragons.roll_die", side_effect=[3, 6, 9, 12, 15, 18])
-    @patch("random.choice", side_effect=["b", "a", "d", "e", "h", "i"])
-    def test_create_character_3(self, mock_letter, mock_roll):
+    # Only test to see if create_character formats a character properly
+    # Functions used in create_character have already been tested in their own unit tests
+    # Therefore values are patched in
+    @patch("dungeonsanddragons.roll_die", side_effect=[3, 3, 3, 3, 3, 3])
+    @patch("dungeonsanddragons.select_class", return_value="monk")
+    @patch("dungeonsanddragons.select_race", return_value="elf")
+    @patch("dungeonsanddragons.roll_hp", return_value=[8, 8])
+    @patch("dungeonsanddragons.generate_name", return_value="Jaxeno")
+    def test_create_character_format(self, mock_name, mock_hp_roll, mock_race, mock_class, mock_roll):
         syllables = 3
-        expected = ["Badehi", ["Strength", 3], ["Dexterity", 6], ["Constitution", 9],
-                              ["Intelligence", 12], ["Wisdom", 15], ["Charisma", 18]]
+        expected = {"Name": "Jaxeno", "Class": "monk", "Race": "elf", "HP": [8, 8], "Strength": 3, "Dexterity": 3,
+                    "Constitution": 3, "Intelligence": 3, "Wisdom": 3, "Charisma": 3, "XP": 0,
+                    "Inventory": []}
         self.assertEqual(expected, create_character(syllables))
