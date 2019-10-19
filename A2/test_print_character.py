b0348e904c8ce878d3d264de5a6a110e61518157
Jason Lui <mrjasonlui@gmail.com>
2019-10-18 20:28:34 -0700

    Added 3 unit tests

--- A2/test_print_character.py
+++ A2/test_print_character.py
@@ -6,56 +6,86 @@ import io
 
 class TestPrint_character(TestCase):
 
+    # A character without an inventory
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_print_character_no_bag(self, mock_output):
-        character = ["Bahedi", ["Strength", 3], ["Dexterity", 6], ["Constitution", 9],
-                     ["Intelligence", 12], ["Wisdom", 15], ["Charisma", 18]]
-        expected_output = f"Your character's name is {character[0]}.\n" \
-                          f"\n" \
-                          f"--Attributes--\n" \
-                          f"{character[1][0]}: {character[1][1]}\n" \
-                          f"{character[2][0]}: {character[2][1]}\n" \
-                          f"{character[3][0]}: {character[3][1]}\n" \
-                          f"{character[4][0]}: {character[4][1]}\n" \
-                          f"{character[5][0]}: {character[5][1]}\n" \
-                          f"{character[6][0]}: {character[6][1]}\n"
-        print_character(character)
-        self.assertEqual(expected_output, mock_output.getvalue())
+    def test_print_character_no_inventory(self, mock_output):
+        character = {"Name": "Ophelia", "Class": "monk", "Race": "elf", "HP": [8, 8], "Strength": 3, "Dexterity": 3,
+                     "Constitution": 3, "Intelligence": 3, "Wisdom": 3, "Charisma": 3, "XP": 0}
+        expected = """
+Your character's name is Ophelia.
 
-    @patch("sys.stdout", new_callable=io.StringIO)
-    def test_print_character_empty_bag(self, mock_output):
-        character = ["Bahedi", ["Strength", 3], ["Dexterity", 6], ["Constitution", 9],
-                     ["Intelligence", 12], ["Wisdom", 15], ["Charisma", 18], []]
-        expected_output = f"Your character's name is {character[0]}.\n" \
-                          f"\n" \
-                          f"--Attributes--\n" \
-                          f"{character[1][0]}: {character[1][1]}\n" \
-                          f"{character[2][0]}: {character[2][1]}\n" \
-                          f"{character[3][0]}: {character[3][1]}\n" \
-                          f"{character[4][0]}: {character[4][1]}\n" \
-                          f"{character[5][0]}: {character[5][1]}\n" \
-                          f"{character[6][0]}: {character[6][1]}\n" \
-                          f"\n--Inventory--\n" \
-                          f"You have no items...\n"
-        print_character(character)
-        self.assertEqual(expected_output, mock_output.getvalue())
+Class: Monk
+Race: Elf
+HP: 8/8
 
-    @patch("sys.stdout", new_callable=io.StringIO)
-    def test_print_character_bag_with_items(self, mock_output):
-        character = ["Bahedi", ["Strength", 3], ["Dexterity", 6], ["Constitution", 9],
-                     ["Intelligence", 12], ["Wisdom", 15], ["Charisma", 18],
-                     ["Boots of Swiftness", "Boots of Lucidity"]]
-        expected_output = f"Your character's name is {character[0]}.\n" \
-                          f"\n" \
-                          f"--Attributes--\n" \
-                          f"{character[1][0]}: {character[1][1]}\n" \
-                          f"{character[2][0]}: {character[2][1]}\n" \
-                          f"{character[3][0]}: {character[3][1]}\n" \
-                          f"{character[4][0]}: {character[4][1]}\n" \
-                          f"{character[5][0]}: {character[5][1]}\n" \
-                          f"{character[6][0]}: {character[6][1]}\n" \
-                          f"\n--Inventory--\n" \
-                          f"Boots of Swiftness\n" \
-                          f"Boots of Lucidity\n"
+--Attributes--
+Strength: 3
+Dexterity: 3
+Constitution: 3
+Intelligence: 3
+Wisdom: 3
+Charisma: 3
+
+EXP: 0
+"""
         print_character(character)
-        self.assertEqual(expected_output, mock_output.getvalue())
+        self.assertEqual(expected, mock_output.getvalue())
+
+    # A character with an inventory but no items
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_print_character_no_items(self, mock_output):
+        character = {"Name": "Ophelia", "Class": "monk", "Race": "elf", "HP": [8, 8], "Strength": 3, "Dexterity": 3,
+                     "Constitution": 3, "Intelligence": 3, "Wisdom": 3, "Charisma": 3, "XP": 0,
+                     "Inventory": []}
+        expected = """
+Your character's name is Ophelia.
+
+Class: Monk
+Race: Elf
+HP: 8/8
+
+--Attributes--
+Strength: 3
+Dexterity: 3
+Constitution: 3
+Intelligence: 3
+Wisdom: 3
+Charisma: 3
+
+EXP: 0
+
+--Inventory--
+You have no items...
+"""
+        print_character(character)
+        self.assertEqual(expected, mock_output.getvalue())
+
+    # A character with an inventory and items
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_print_character_items(self, mock_output):
+        character = {"Name": "Ophelia", "Class": "monk", "Race": "elf", "HP": [8, 8], "Strength": 3, "Dexterity": 3,
+                     "Constitution": 3, "Intelligence": 3, "Wisdom": 3, "Charisma": 3, "XP": 0,
+                     "Inventory": ["Boots of Swiftness", "Boots of Alacrity"]}
+        expected = """
+Your character's name is Ophelia.
+
+Class: Monk
+Race: Elf
+HP: 8/8
+
+--Attributes--
+Strength: 3
+Dexterity: 3
+Constitution: 3
+Intelligence: 3
+Wisdom: 3
+Charisma: 3
+
+EXP: 0
+
+--Inventory--
+Boots of Swiftness
+Boots of Alacrity
+"""
+        print_character(character)
+        self.assertEqual(expected, mock_output.getvalue())
