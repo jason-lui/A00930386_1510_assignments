73ddf995159a85df18f63c8a627ac0b0a8cb6818
Jason Lui <mrjasonlui@gmail.com>
2019-11-05 16:53:23 -0800

    Made the basic creation function

---
+++ A3/monster.py
@@ -0,0 +1,12 @@
+import doctest
+
+
+def monster():
+    """
+    Generate a monster with 5 HP.
+
+    :postcondition: a monster with 5 HP will be generated as a dictionary
+    :return: a monster with 5 HP represented as a dictionary
+    """
+    monster_info = {"HP": 10}
+    return monster_info
