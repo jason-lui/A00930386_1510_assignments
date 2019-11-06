591589868dff9ca50c5515c64baabcec8c586b81
Jason Lui <mrjasonlui@gmail.com>
2019-11-05 17:13:33 -0800

    Deleted make_character because create_character is the same thing

--- A3/test_make_character.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from maze import make_character
-
-
-class TestMake_character(TestCase):
-
-    def test_make_character(self):
-        expected = {'coords': (0, 0)}
-        self.assertEqual(expected, make_character())
