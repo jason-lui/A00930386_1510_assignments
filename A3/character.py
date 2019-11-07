90a64aa67bb748cb90d732ce37907a43799131a1
Jason Lui <mrjasonlui@gmail.com>
2019-11-06 18:02:16 -0800

    Fixed HP representation

--- A3/character.py
+++ A3/character.py
@@ -12,7 +12,7 @@ def create_character():
 
     char_info['Name'] = "Link"
     char_info['coords'] = 10
-    char_info['HP'] = 10
+    char_info['HP'] = [10, 10]
     char_info['Power'] = 6
     return char_info
 
