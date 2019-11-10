ce9582e6610916976b4fa749e4825a59380f3f71
Jason Lui <mrjasonlui@gmail.com>
2019-11-10 06:38:17 -0800

    Changed room description

--- A3/sud.py
+++ A3/sud.py
@@ -218,59 +218,59 @@ def describe(my_char):
     :precondition: my_char must be a dictionary with a set of coordinates
     :postcondition: a description of the current room will be printed
     """
-    board_descriptions = {(0, 0): 'You wake up. You realize that you are in a maze of some sort. There are 2 doorways '
-                                  'in this room: south and east.',
-                          (0, 1): 'You enter a dimly lit room. You see some symbols on the wall and a broken sword.',
-                          (0, 2): 'You slowly walk into a room. There was a trap hole! You narrowly avoid it. Phew!',
-                          (0, 3): 'You find a room with some rats. Despite being rats, they\'re quite friendly.',
-                          (0, 4): 'You are the corner of the maze. Nothing interesting here.',
-                          (1, 0): 'You find a treasure chest in this room. You open the chest. Only a few gold '
-                                  'coins... Bummer.',
-                          (1, 1): 'You find a treasure chest in this room. You open the chest. You find a new car! '
-                                  'You don\'t have your license though.',
-                          (1, 2): 'You find a treasure chest in this room. You open the chest. You find purpose in '
-                                  'life. Wow, that was deep!',
-                          (1, 3): 'You find a treasure chest in this room. You open the chest. Empty... Rats!',
-                          (1, 4): 'You find a treasure chest in this room. You open the chest. It\'s full of treasure! '
-                                  'Gems, jewellery, gold bars, doubloons, and trinkets. How lucky!',
-                          (2, 0): 'You decide to continue on the top side of the maze. You find some writing on the '
-                                  'wall. It says "The exit is at (4, ...". You can\'t make out the rest.',
-                          (2, 1): 'You enter a room with a table with bread and water. The bread looks stale, but the '
-                                  'water is clean... or at least you think it is. You eat the bread anyways and you '
-                                  'take a mouthful of water.',
-                          (2, 2): 'You are at the center of maze now. You see a skeleton with a book next to it. '
-                                  'You open the book. In large sans serif font "DO YOUR UNIT TESTS OR YOU WILL '
-                                  'END UP LIKE ME". Yeesh! You drop the book.',
-                          (2, 3): 'You enter a room with some lit candles. These candles look like they\'ve been '
-                                  'burning for a while now.',
-                          (2, 4): 'You walk along the east side of the maze. You hear a noise that sounds like '
-                                  'chatting. You wonder if someone else is in here with you.',
-                          (3, 0): 'You walk along the west side of the maze. You see a window to the outside world. '
-                                  'You try to squeeze through the window. You\'re too big to make it through. Darn it! '
-                                  'You shouldn\'t have eaten so many donuts.',
-                          (3, 1): 'You fall into a trap hole! You feel around the dark for anything to climb on. '
-                                  'You press a button that drops a rope down from the ceiling. You climb out of the '
-                                  'pit. Phew that was close.',
-                          (3, 2): 'You step on a pressure plate as your enter this room. A large boulder falls from '
-                                  'the ceiling. Oh no! You narrowly evade the falling boulder. Phew that was a close '
-                                  'one.',
-                          (3, 3): 'You enter a bedroom. You see a large comfy bed. You decide to take a nap in it. '
-                                  'You sleep for a bit and wake up refreshed.',
-                          (3, 4): 'You enter a room with a black cat. He\'s friendly. He nudges against you, purring. '
-                                  'You wonder whose cat this is.',
-                          (4, 0): 'You\'re at the southwest corner of the maze. Nothing interesting here.',
-                          (4, 1): 'You walk along the south side of the maze. You feel like the exit is near. '
-                                  'You get careless and walk into a spiderweb. AH! You hate spiders.',
-                          (4, 2): 'You walk along the south side of the maze. You feel a light breeze. You think the '
-                                  'exit might be nearby. You look at your surroundings. You see a mirror. You see '
-                                  'your reflection. You see a face that you don\'t recognize. You look muscular and '
-                                  'manly. You realize you\'re not in your own body. You think about how you will '
-                                  'explain this to your friends and family.',
-                          (4, 3): 'You enter a room full of skeletons. You wonder what happened to these people. '
-                                  'You see a note on the floor. You read the note. "Watch out for the Goblins". '
-                                  'You see the exit of the maze to your east.',
-                          (4, 4): 'You find yourself in an open field. The grass is thick. You look around to see if '
-                                  'there are any signs of civilization. No luck.'}
+    board_descriptions = {(0, 0): 'You wake up. You realize that you are in a maze of some sort. There are 2 \n'
+                                  'doorways in this room: south and east.\n',
+                          (0, 1): 'You enter a dimly lit room. You see some symbols on the wall and a broken sword.\n',
+                          (0, 2): 'You slowly walk into a room. There was a trap hole! You narrowly avoid it. Phew!\n',
+                          (0, 3): 'You find a room with some rats. Despite being rats, they\'re quite friendly.\n',
+                          (0, 4): 'You are the corner of the maze. Nothing interesting here.\n',
+                          (1, 0): 'You find a treasure chest in this room. You open the chest. Only a few gold \n'
+                                  'coins... Bummer.\n',
+                          (1, 1): 'You find a treasure chest in this room. You open the chest. You find a new car! \n'
+                                  'You don\'t have your license though.\n',
+                          (1, 2): 'You find a treasure chest in this room. You open the chest. You find purpose in \n'
+                                  'life. Wow, that was deep!\n',
+                          (1, 3): 'You find a treasure chest in this room. You open the chest. Empty... Rats!\n',
+                          (1, 4): 'You find a treasure chest in this room. You open the chest. It\'s full of \n'
+                                  'treasure! Gems, jewellery, gold bars, doubloons, and trinkets. How lucky!\n',
+                          (2, 0): 'You decide to continue on the top side of the maze. You find some writing on the \n'
+                                  'wall. It says "The exit is at (4, ...". You can\'t make out the rest.\n',
+                          (2, 1): 'You enter a room with a table with bread and water. The bread looks stale, but \n'
+                                  'the water is clean... or at least you think it is. You eat the bread anyways and \n'
+                                  'you take a mouthful of water.\n',
+                          (2, 2): 'You are at the center of maze now. You see a skeleton with a book next to it. \n'
+                                  'You open the book. In large sans serif font "DO YOUR UNIT TESTS OR YOU WILL \n'
+                                  'END UP LIKE ME". Yeesh! You drop the book.\n',
+                          (2, 3): 'You enter a room with some lit candles. These candles look like they\'ve been \n'
+                                  'burning for a while now.\n',
+                          (2, 4): 'You walk along the east side of the maze. You hear a noise that sounds like \n'
+                                  'chatting. You wonder if someone else is in here with you.\n',
+                          (3, 0): 'You walk along the west side of the maze. You see a window to the outside world. \n'
+                                  'You try to squeeze through the window. You\'re too big to make it through. Darn \n'
+                                  'it! You shouldn\'t have eaten so many donuts.\n',
+                          (3, 1): 'You fall into a trap hole! You feel around the dark for anything to climb on. \n'
+                                  'You press a button that drops a rope down from the ceiling. You climb out of the \n'
+                                  'pit. Phew that was close.\n',
+                          (3, 2): 'You step on a pressure plate as your enter this room. A large boulder falls from \n'
+                                  'the ceiling. Oh no! You narrowly evade the falling boulder. Phew that was a close \n'
+                                  'one.\n',
+                          (3, 3): 'You enter a bedroom. You see a large comfy bed. You decide to take a nap in it. \n'
+                                  'You sleep for a bit and wake up refreshed.\n',
+                          (3, 4): 'You enter a room with a black cat. He\'s friendly. He nudges against you, \n'
+                                  'purring. You wonder whose cat this is.\n',
+                          (4, 0): 'You\'re at the southwest corner of the maze. Nothing interesting here.\n',
+                          (4, 1): 'You walk along the south side of the maze. You feel like the exit is near. \n'
+                                  'You get careless and walk into a spiderweb. AH! You hate spiders.\n',
+                          (4, 2): 'You walk along the south side of the maze. You feel a light breeze. You think the \n'
+                                  'exit might be nearby. You look at your surroundings. You see a mirror. You see \n'
+                                  'your reflection. You see a face that you don\'t recognize. You look muscular and \n'
+                                  'manly. You realize you\'re not in your own body. You think about how you will \n'
+                                  'explain this to your friends and family.\n',
+                          (4, 3): 'You enter a room full of skeletons. You wonder what happened to these people. \n'
+                                  'You see a note on the floor. You read the note. "Watch out for the Goblins". \n'
+                                  'You see the exit of the maze to your east.\n',
+                          (4, 4): 'You find yourself in an open field. The grass is thick. You look around to see if \n'
+                                  'there are any signs of civilization. No luck.\n'}
     key = my_char['coords']
     print(board_descriptions[key])
 
