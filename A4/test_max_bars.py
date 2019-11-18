45662c373c0e32f86132e8f174770e4ddc9e206c
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 15:32:40 -0800

    Finished unit tests

--- A4/test_max_bars.py
+++ A4/test_max_bars.py
@@ -4,5 +4,20 @@ from question_8 import max_bars
 
 class TestMax_bars(TestCase):
 
-    def test_max_bars(self):
-        self.fail()
+    def test_max_bars_1_12(self):
+        test_lower_bound = 1
+        test_upper_bound = 12
+        expected = 10
+        self.assertEqual(expected, max_bars(test_lower_bound, test_upper_bound))
+
+    def test_max_bars_0_5(self):
+        test_lower_bound = 0
+        test_upper_bound = 5
+        expected = 0
+        self.assertEqual(expected, max_bars(test_lower_bound, test_upper_bound))
+
+    def test_max_bars_0_9(self):
+        test_lower_bound = 0
+        test_upper_bound = 9
+        expected = 8
+        self.assertEqual(expected, max_bars(test_lower_bound, test_upper_bound))
