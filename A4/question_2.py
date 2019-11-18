c6b1c1f36103d41a1f25857245e1a5e2038c52b2
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 11:52:58 -0800

    Finished doctests

--- A4/question_2.py
+++ A4/question_2.py
@@ -1,3 +1,6 @@
+import doctest
+
+
 def gcd(a: int, b: int) -> int:
     """
     Determine the greatest common denominator (GCD) of 2 integers.
@@ -8,16 +11,33 @@ def gcd(a: int, b: int) -> int:
     :precondition: a must be a non-zero integer
     :postcondition: the GCD will be calculated
     :return: the GCD as an integer
+
+    >>> gcd(0, 0)
+    Traceback (most recent call last):
+    ...
+    Exception: Both a and b cannot be 0.
+    >>> gcd(5, 0)
+    5
+    >>> gcd(-5, 0)
+    -5
+    >>> gcd(30, 12)
+    6
+    >>> gcd(123, 36)
+    3
     """
+    # Raise exception if both a and b are 0
+    if not a and not b:
+        raise Exception("Both a and b cannot be 0.")
+
     # If either a or b are 0, but not both return the non-zero
     if not a or not b:
         return a if a else b
 
     while a % b != 0:
-        # Set a as the result of a % b
-        a %= b
-
-        # Swap the values of a and b for consistency
-        a, b = b, a
-
+        a %= b  # Set a as the result of a % b
+        a, b = b, a  # Swap the values of a and b for consistency
     return b
+
+
+if __name__ == '__main__':
+    doctest.testmod()
