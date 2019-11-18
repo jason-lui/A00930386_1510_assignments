0c8dc5b86249ff920fc26cccc6423cb93c0dbca6
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 12:05:47 -0800

    Finished unit tests

---
+++ A4/test_gcd.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+from question_2 import gcd
+
+
+class TestGcd(TestCase):
+
+    def test_gcd_exception(self):
+        test_a = 0
+        test_b = 0
+        expected = Exception
+        self.assertRaises(expected, gcd, test_a, test_b)
+
+    def test_gcd_exception_5_0(self):
+        test_a = 5
+        test_b = 0
+        expected = 5
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_exception_neg5_0(self):
+        test_a = -5
+        test_b = 0
+        expected = -5
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_exception_30_12(self):
+        test_a = 30
+        test_b = 12
+        expected = 6
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_exception_36_123(self):
+        test_a = 36
+        test_b = 123
+        expected = 3
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_exception_neg36_123(self):
+        test_a = -36
+        test_b = 123
+        expected = -3
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_exception_neg36_neg123(self):
+        test_a = -36
+        test_b = -123
+        expected = -3
+        self.assertEqual(expected, gcd(test_a, test_b))
