d67c9ed291d55b423df8d5f44bf50ba9b8c00666
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 14:23:52 -0800

    Finished unit tests

---
+++ A4/test_selection_sort.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from question_4 import selection_sort
+
+
+class TestSelection_sort(TestCase):
+
+    def test_selection_sort_exception(self):
+        test_list = []
+        expected = Exception
+        self.assertRaises(expected, selection_sort, test_list)
+
+    def test_selection_sort_integers(self):
+        test_list = [3, 1, 2, 0]
+        expected = [0, 1, 2, 3]
+        self.assertEqual(expected, selection_sort(test_list))
+
+    def test_selection_sort_strings(self):
+        test_list = ['bae', 'Bae', 'xX']
+        expected = ['Bae', 'bae', 'xX']
+        self.assertEqual(expected, selection_sort(test_list))
+
+    def test_selection_sort_floats(self):
+        test_list = [3.3, 1.1, 2.2]
+        expected = [1.1, 2.2, 3.3]
+        self.assertEqual(expected, selection_sort(test_list))
+
+    def test_selection_sort_booleans(self):
+        test_list = [False, True, False]
+        expected = [False, False, True]
+        self.assertEqual(expected, selection_sort(test_list))
