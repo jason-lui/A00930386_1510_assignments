8d98ef383b2a79b6a82bfb49aeedc43dace5d35f
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 11:29:50 -0800

    Edited function descriptions

--- A4/question_2.py
+++ A4/question_2.py
@@ -1,9 +1,13 @@
 def gcd(a: int, b: int) -> int:
     """
+    Determine the greatest common denominator (GCD) of 2 integers.
 
-    :param a:
-    :param b:
-    :return:
+    :param a: an integer
+    :param b: an integer
+    :precondition: a must be a non-zero integer
+    :precondition: a must be a non-zero integer
+    :postcondition: the GCD will be calculated
+    :return: the GCD as an integer
     """
     # If either a or b are 0, but not both return the non-zero
     if not a or not b:
