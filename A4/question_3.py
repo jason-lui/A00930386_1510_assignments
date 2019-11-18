56b7af476d9e6e1504bbbf8c65c239b567418d82
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 12:32:42 -0800

    Finished docstring and made code more pythonic using enumerate

--- A4/question_3.py
+++ A4/question_3.py
@@ -1,22 +1,23 @@
-def dijkstra(arr: list):
+def dijkstra(dutch_colors: list):
     """
+    Sort a list of 'red', 'white' and 'blue' strings based on the Dutch flag partition.
 
-    :param arr:
-    :return:
+    :param dutch_colors: a list
+    :precondition: dutch_colors must be a list of strings containing only 'red', 'white' and 'blue' strings
+    :postcondition: the list will be sorted by 'red', 'white', 'blue' in place
     """
     red = 0
-    blue = len(arr) - 1
+    blue = len(dutch_colors) - 1
 
-    for i, color in enumerate(arr):
+    # Sort 'red's to the beginning of the list
+    for i, color in enumerate(dutch_colors):
         if color == 'red':
-            arr[i], arr[red] = arr[red], arr[i]
-            red += 1
-    for i in reversed(range(len(arr))):
-        if arr[i] == 'blue':
-            arr[i], arr[blue] = arr[blue], arr[i]
-            blue -= 1
+            dutch_colors[i], dutch_colors[red] = dutch_colors[red], dutch_colors[i]
+            red += 1  # Increment write index
 
-
-# dutch = ['white', 'blue', 'blue', 'red', 'white', 'red', 'white']
-# dijkstra(dutch)
-# print(dutch)
+    # Sort 'blue's to the end of the list
+    for i, color in enumerate(reversed(dutch_colors)):
+        i = len(dutch_colors) - 1 - i  # Generate indices to iterate backwards through the list
+        if color == 'blue':
+            dutch_colors[i], dutch_colors[blue] = dutch_colors[blue], dutch_colors[i]
+            blue -= 1  # Decrement write index
