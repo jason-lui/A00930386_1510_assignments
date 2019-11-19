252908569e116b97c1f741037821c79f92417f91
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 16:36:04 -0800

    Refactored function

--- A4/question_7.py
+++ A4/question_7.py
@@ -1,30 +1,50 @@
-
+import doctest
 
 
 def calorie_counter():
     """
+    Assemble and print a list of food items, total calorie count, and the average calorie count.
 
-    :return:
+    :postcondition: the food list will be assembled and printed
     """
-    calories = {"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66,
+    food_items = {"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66,
                  "pasta": 221, "rice": 225, "milk": 122, "cheese": 115,
                  "yogurt": 145, "beef": 240, "chicken": 140, "butter": 102}
 
     # Input loop
-    new_item = input("Enter food item to add, or ’q’ to exit: ")
+    new_item = input("Enter food item to add, or 'q' to exit: ")
     while new_item != "q":
+        # Update dictionary with the new food item and its caloric value
         new_item_calories = int(input(f"Enter calories for {new_item}: "))
-        calories[new_item] = new_item_calories
-        total_calories = 0
-        food_item_names = []
+        food_items[new_item] = new_item_calories
 
-        for item in calories:
-            total_calories += calories[item]
-            food_item_names.append(item)
+        # Print food list, total calories, and average calories
+        print_food_calories(food_items)
 
-        avg_calories = total_calories / len(calories)
+        # Ask for the next item or quit
+        new_item = input("Enter food item to add, or 'q' to exit: ")
 
-        print("\nFood Items:", sorted(food_item_names))
-        print(f"Total Calories: {total_calories} Average Calories: %0.1f\n" % avg_calories)
 
-        new_item = input("Enter food item to add, or ’q’ to exit: ")
+def print_food_calories(food_list: dict):
+    """
+    Print the a sorted list of foods, its total calorie count, and its average calorie per item.
+
+    :param food_list: a dictionary
+    :precondition: food_list must be a dictionary of food items as strings and their caloric values as ints
+    :postcondition: a sorted list of foods, a total calorie count, and an average calorie count will be printed
+
+    >>> print_food_calories({"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66})
+    <BLANKLINE>
+    Food Items: ['apple', 'bread', 'carrot', 'lettuce']
+    Total Calories: 195 Average Calories: 48.8
+    <BLANKLINE>
+    """
+    total_calories = sum([value for value in food_list.values()])
+    avg_calories = total_calories / len(food_list)
+
+    print("\nFood Items:", sorted(food_list.keys()))
+    print("Total Calories:", total_calories, "Average Calories: %0.1f\n" % avg_calories)
+
+
+if __name__ == '__main__':
+    doctest.testmod()
