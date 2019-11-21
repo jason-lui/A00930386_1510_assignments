59d7b9a88e74e63021fa129cf0c5ff4307dce1b0
Jason Lui <mrjasonlui@gmail.com>
2019-11-21 14:22:40 -0800

    Added unit tests

--- A4/test_gcd.py
+++ A4/test_gcd.py
@@ -22,26 +22,37 @@ class TestGcd(TestCase):
         expected = ZeroDivisionError
         self.assertRaises(expected, gcd, test_a, test_b)
 
-    def test_gcd_exception_30_12(self):
+    def test_gcd_30_12(self):
         test_a = 30
         test_b = 12
         expected = 6
         self.assertEqual(expected, gcd(test_a, test_b))
 
-    def test_gcd_exception_36_123(self):
+    def test_gcd_36_123(self):
         test_a = 36
         test_b = 123
         expected = 3
         self.assertEqual(expected, gcd(test_a, test_b))
 
-    def test_gcd_exception_neg36_123(self):
+    def test_gcd_neg36_123(self):
         test_a = -36
         test_b = 123
         expected = 3
         self.assertEqual(expected, gcd(test_a, test_b))
 
-    def test_gcd_exception_neg36_neg123(self):
+    def test_gcd_neg36_neg123(self):
         test_a = -36
         test_b = -123
         expected = 3
         self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_same_num(self):
+        test_a = 7
+        test_b = 7
+        expected = 7
+        self.assertEqual(expected, gcd(test_a, test_b))
+
+    def test_gcd_alias(self):
+        test_a = 7
+        expected = 7
+        self.assertEqual(expected, gcd(test_a, test_a))
