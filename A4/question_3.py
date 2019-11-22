ac90a6ef26a3bcef092d24dbbde2f85aa59c6497
Jason Lui <mrjasonlui@gmail.com>
2019-11-21 23:06:37 -0800

    Made code more pythonic

--- A4/question_3.py
+++ A4/question_3.py
@@ -1,23 +1,23 @@
-def dijkstra(dutch_colors: list):
+def dijkstra(dutch: list):
     """
     Sort a list of 'red', 'white' and 'blue' strings based on the Dutch flag partition.
 
-    :param dutch_colors: a list
+    :param dutch: a list
     :precondition: dutch_colors must be a list of strings containing only 'red', 'white' and 'blue' strings
     :postcondition: the list will be sorted by 'red', 'white', 'blue' in place
     """
-    red = 0
-    blue = len(dutch_colors) - 1
+    red, mid, blue = 0, 0, len(dutch) - 1
+    while mid < blue:
+        # Element is red, sort it to the beginning of the list
+        if dutch[mid] == "red":
+            dutch[mid], dutch[red] = dutch[red], dutch[mid]
+            red, mid = red + 1, mid + 1
 
-    # Sort 'red's to the beginning of the list
-    for i, color in enumerate(dutch_colors):
-        if color == 'red':
-            dutch_colors[i], dutch_colors[red] = dutch_colors[red], dutch_colors[i]
-            red += 1  # Increment write index
+        # Element is white, leave it
+        elif dutch[mid] == "white":
+            mid += 1
 
-    # Sort 'blue's to the end of the list
-    for i, color in enumerate(reversed(dutch_colors)):
-        i = len(dutch_colors) - 1 - i  # Generate indices to iterate backwards through the list
-        if color == 'blue':
-            dutch_colors[i], dutch_colors[blue] = dutch_colors[blue], dutch_colors[i]
-            blue -= 1  # Decrement write index
+        # Element is blue, sort it to the end of the list
+        elif dutch[mid] == "blue":
+            dutch[mid], dutch[blue] = dutch[blue], dutch[mid]
+            blue -= 1
