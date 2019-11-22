4360d8b7f2dd9cf0a35b570e21d980c706ed258a
Jason Lui <mrjasonlui@gmail.com>
2019-11-21 23:21:36 -0800

    Edited comments

--- A4/question_6.py
+++ A4/question_6.py
@@ -1,8 +1,8 @@
 """
-#1) Lines do not intersect
-#2) Lines are coincident
-#3) Lines intersect and both have negative slopes
-#4) Lines intersect and both have positive slopes
-#5) Lines intersect and one has a negative slope while the other has a positive slope
-#6) Lines intersect and one line is horizontal and the other is vertical
+#1) Test 2 parallel lines that do not intersect.
+#2) Test 2 identical lines that are coincident.
+#3) Test 2 lines that intersect and both have negative slopes.
+#4) Test 2 lines that intersect and both have positive slopes
+#5) Test 2 lines that intersect and one has a negative slope while the other has a positive slope
+#6) Test 2 lines that intersect and one line is horizontal and the other is vertical
 """
