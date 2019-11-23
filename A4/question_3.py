26d599e6ead01acaf430ce978f97160cf0320bdf
Jason Lui <mrjasonlui@gmail.com>
2019-11-23 14:07:44 -0800

    Fixed a bug

--- A4/question_3.py
+++ A4/question_3.py
@@ -6,8 +6,8 @@ def dijkstra(dutch: list):
     :precondition: dutch_colors must be a list of strings containing only 'red', 'white' and 'blue' strings
     :postcondition: the list will be sorted by 'red', 'white', 'blue' in place
     """
-    red, mid, blue = 0, 0, len(dutch) - 1
-    while mid <= blue:
+    red, mid, blue = 0, 0, len(dutch)
+    while mid < blue:
         # Element is red, sort it to the beginning of the list
         if dutch[mid] == "red":
             dutch[mid], dutch[red] = dutch[red], dutch[mid]
@@ -19,5 +19,5 @@ def dijkstra(dutch: list):
 
         # Element is blue, sort it to the end of the list
         elif dutch[mid] == "blue":
-            dutch[mid], dutch[blue] = dutch[blue], dutch[mid]
             blue -= 1
+            dutch[mid], dutch[blue] = dutch[blue], dutch[mid]
